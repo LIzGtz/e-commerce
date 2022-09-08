4ab3a136146dd515c1bfea3c027e37e94a6296cd
Lizeth Gutierrez <lizethd.gutierrez11@gmail.com>
2022-09-08 11:33:29 -0500

    Logeo de usuario

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { useDispatch } from 'react-redux';
 import './Login.css'
 
 export const Login = () => {
+  const [ register, handleSubmit ] = useForm();
+  const dispatch = useDispatch();
+
+  const submitLogin = () => {
+    dispatch(loginThunk)
+  }
+
   return (
     <div className='content'>
       <div className='login-container'>
         <div className='main-container'>
-          <form className='login'>
+          <form className='login' onSubmit={handleSubmit(submitLogin)}>
             <strong>Welcome! Enter your email and password to continue</strong>
             <p className='login-message'></p>
             <div className='input-container'>
@@ -18,7 +27,7 @@ export const Login = () => {
               <input id='password' type='password' name='password'></input>
             </div>
             <div className='error-message hidden'></div>
-            <button className='submit-button'>Login</button>
+            <button type='submit' className='submit-button'>Login</button>
             <div className='switch-forms'>
               Don't have an account?&nbsp;
               <button type='button'>Sign-up</button>
